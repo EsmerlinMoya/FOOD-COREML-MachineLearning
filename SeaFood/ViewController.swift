814dbfa5248dc702773912963706c5b6926260e0
merlin Moya <Esmerlinmoya@gmail.com>
2022-02-15 14:45:02 -0500

    Food Project using CORE ML and Machine Learning Example

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imaveView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+       // imagePicker.sourceType = .camera //if you are testing the app using your iphone
+        imagePicker.sourceType = .photoLibrary //if you are using a simulator
+        imagePicker.allowsEditing = false
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { imaveView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
 }
 
